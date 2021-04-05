7eee21fff0d510ef622e5a40f48efd2b02d4cb2c
Norman Lim <normanlys16@gmail.com>
2021-04-05 14:59:33 +0800

    Fixed pattern_matching

--- 2020/1A/pattern_matching.py
+++ 2020/1A/pattern_matching.py
@@ -1,4 +1,4 @@
-from typing import List, Pattern
+from typing import List
 
 
 def solve(arr: List[str]) -> str:
@@ -14,43 +14,39 @@ def solve(arr: List[str]) -> str:
         splitted_patterns = pattern.split("*")
 
         if pattern[0] != "*":
-            prefixes.append(splitted_patterns[0])
-            if len(splitted_patterns[0]) > len(prefixes[i]):
+            current_prefix = splitted_patterns[0]
+            prefixes.append(current_prefix)
+            if len(current_prefix) > len(prefixes[lp]):
                 lp = i
-            pattern = pattern[len(splitted_patterns[0]):]
+            pattern = pattern[len(current_prefix):]
         else:
             prefixes.append("")
             
         if pattern[-1] != "*":
-            suffixes.append(splitted_patterns[-1])
-            if len(splitted_patterns[0]) > len(suffixes[i]):
+            current_suffix = splitted_patterns[-1]
+            suffixes.append(current_suffix)
+            if len(current_suffix) > len(suffixes[ls]):
                 ls = i
-            pattern = pattern[:len(pattern)-len(splitted_patterns[-1])]
+            pattern = pattern[:len(pattern)-len(current_suffix)]
         else:
             suffixes.append("")
 
         p.append(pattern)
-    print(prefixes)
-    print(suffixes)
+
     for i in range(n):
         if prefixes[i] and prefixes[i] != prefixes[lp][:len(prefixes[i])]:
-            print(f"p {i}")
             return "*"
         if suffixes[i] and suffixes[i] != suffixes[ls][len(suffixes[ls])-len(suffixes[i]):len(suffixes[ls])]:
-            print(f"s {suffixes[ls][len(suffixes[ls])-len(suffixes[i]):len(suffixes[ls])]}")
             return "*"
 
     result = prefixes[lp]
     for pattern in p:
         for sp in pattern.split("*"):
             result += sp
-    result += suffixes[lp]
+    result += suffixes[ls]
     return result
 
 
-import sys
-
-sys.stdin = open("test.txt", "r")
 T = int(input())
 for i in range(T):
     N = int(input())
